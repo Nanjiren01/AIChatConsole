10a615665cd6d48dfe323d7aa6f845db1ec0d75e
tanqicun-mac <411707420@qq.com>
2023-05-24 11:04:33 +0800

    global config(website registerTypes)

--- src/views/website/config/index.vue
+++ src/views/website/config/index.vue
@@ -22,6 +22,19 @@
             <el-form-item label="注册页副标题">
               <el-input v-model="form.registerPageSubTitle" />
             </el-form-item>
+            <el-form-item label="注册方式">
+              <el-alert
+                title="当前版本仅支持选择一种注册方式"
+                type="info"
+                style="line-height: 14px;margin-bottom: 5px;"
+                :closable="false"
+              />
+              <el-checkbox-group v-model="form.registerTypes" @change="handleCheckedRegisterTypesChange">
+                <el-checkbox label="OnlyUsername">仅用户名</el-checkbox>
+                <el-checkbox label="OnlyUsernameWithCaptcha">仅用户名（带图形验证码）</el-checkbox>
+                <el-checkbox label="UsernameAndEmailWithCaptchaAndCode">用户名+邮箱验证码（带图形验证码）</el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
             <el-form-item>
               <el-button type="primary" :disabled="loading" @click="handleSubmit">提 交</el-button>
             </el-form-item>
@@ -44,7 +57,8 @@ export default {
         title: '',
         subTitle: '',
         loginPageSubTitle: '',
-        registerPageSubTitle: ''
+        registerPageSubTitle: '',
+        registerTypes: []
       },
       loading: false
     }
@@ -62,22 +76,34 @@ export default {
         this.form.subTitle = config.websiteContent.subTitle
         this.form.loginPageSubTitle = config.websiteContent.loginPageSubTitle
         this.form.registerPageSubTitle = config.websiteContent.registerPageSubTitle
+
+        const types = config.websiteContent.registerTypes
+        this.form.registerTypes.splice(0, this.form.registerTypes.length)
+        this.form.registerTypes.push(types && types.length ? types[0] : 'OnlyUsername')
       }).finally(() => {
         this.loading = false
       })
     },
     handleSubmit() {
+      if (this.form.registerTypes.length > 1) {
+        this.$message.error('当前版本仅支持选择一种注册方式！')
+        return
+      }
       this.loading = true
       saveWebsiteConfig({
         title: this.form.title,
         subTitle: this.form.subTitle,
         loginPageSubTitle: this.form.loginPageSubTitle,
-        registerPageSubTitle: this.form.registerPageSubTitle
+        registerPageSubTitle: this.form.registerPageSubTitle,
+        registerTypes: this.form.registerTypes
       }).then(() => {
         this.$message.success('修改成功！')
       }).finally(() => {
         this.loading = false
       })
+    },
+    handleCheckedRegisterTypesChange(newTypes) {
+      // console.log('newTypes', newTypes)
     }
   }
 }
