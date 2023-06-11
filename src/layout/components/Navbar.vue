f6fbf0abcfb2b837f7cf3eda88c69ec344c5b7ce
tanqicun-mac <411707420@qq.com>
2023-06-11 23:58:46 +0800

    add ai-chat-bot.png

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -22,7 +22,7 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img src="@/assets/ai-chat-bot.png" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
