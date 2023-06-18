74c66d6dbf4b64bc825b6f58e6b6d5dc454a55b3
tanqicun-mac <411707420@qq.com>
2023-06-18 23:50:17 +0800

    v0.2.1: remove 带图形验证码

--- src/views/website/config/index.vue
+++ src/views/website/config/index.vue
@@ -29,7 +29,7 @@
               <el-checkbox-group v-model="form.registerTypes" @change="handleCheckedRegisterTypesChange">
                 <el-checkbox label="OnlyUsername">仅用户名</el-checkbox>
                 <el-checkbox label="OnlyUsernameWithCaptcha">仅用户名（带图形验证码）</el-checkbox>
-                <el-checkbox label="UsernameAndEmailWithCaptchaAndCode">用户名+邮箱验证码（带图形验证码）</el-checkbox>
+                <el-checkbox label="UsernameAndEmailWithCaptchaAndCode">用户名+邮箱验证码</el-checkbox>
               </el-checkbox-group>
             </el-form-item>
             <el-form-item label="聊天页副标题">
