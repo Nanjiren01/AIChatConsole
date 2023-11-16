69f0774732be5250add82f3692f29313d43d7322
tanqicun-mac <411707420@qq.com>
2023-11-16 21:31:19 +0800

    change version to 0.11

--- src/views/info/about.vue
+++ src/views/info/about.vue
@@ -7,7 +7,7 @@
       </div>
       <div style="font-size: 14px; line-height: 20px; ">
         <div>Admin：v{{ version }}</div>
-        <div>Console: v0.9.5</div>
+        <div>Console: v0.11</div>
       </div>
       <div v-html="about" />
     </div>
